b8dcf01614186758c4142cbfc4d648ed060c183c
GautamMarkande <118103083+GautamMarkande@users.noreply.github.com>
2024-01-16 11:23:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,10 +2,16 @@
 const nameInput = document.getElementById("name");
 const scoreInput = document.getElementById("score");
 const scores = document.getElementById("scores");
+const tbody = document.getElementById("tbody");
 
 // Save score to Local Storage
 function saveScore() {
   // complete the code here
+	const larr = [];
+	localStorage.setItem('arr',JOSN.stringify(arr))
+	const arr = [];
+	
+	localStorage.setItem('arr',[{name:}])
   showScores();
 }
 
